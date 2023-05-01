91a5ef91918272b7a30dc9a115a4688281a0a93d
Nderim <nderim.topalli@plana.earth>
2023-05-01 16:47:44 +0200

    Fix mobile

--- src/lib/components/step.svelte
+++ src/lib/components/step.svelte
@@ -5,9 +5,11 @@
 	export let index;
 </script>
 
-<div class="flex w-full items-center even:flex-row-reverse mt-20 mb-20">
+<div
+	class="flex w-full items-center flex-col text-center sm:text-left sm:even:flex-row-reverse sm:odd:flex-row mt-20 mb-20 p-10 sm:p-0"
+>
 	<img src={stepData.img} alt="" class="w-48 h-48" />
-	<div class={`${isEven ? 'ml-16' : 'mr-1ml-16'}`}>
+	<div class="mt-10 sm:mt-0 {`${isEven ? 'sm:ml-16' : 'sm:mr-1ml-16'}`}">
 		<span class="text-sm text-indigo-400">step {index + 1}</span>
 		<h2 class="font-extrabold text-xl">{stepData.title}</h2>
 		<div>{stepData.description}</div>
